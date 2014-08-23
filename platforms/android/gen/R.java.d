029c15093a59dcddaef96ef2cbdd8a9bebb9fd29
Sean Brown <sdbmich@yahoo.com>
2014-08-23 12:28:47 -0700

    cleaned up library files

--- platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-c:\RoRDev\pxbMobile\platforms\android\gen\com\pixiboard\pxbMobile\R.java \
- : c:\RoRDev\pxbMobile\platforms\android\res\drawable\icon.png \
-c:\RoRDev\pxbMobile\platforms\android\res\drawable-hdpi\icon.png \
-c:\RoRDev\pxbMobile\platforms\android\res\drawable-ldpi\icon.png \
-c:\RoRDev\pxbMobile\platforms\android\res\drawable-mdpi\icon.png \
-c:\RoRDev\pxbMobile\platforms\android\res\drawable-xhdpi\icon.png \
-c:\RoRDev\pxbMobile\platforms\android\res\values\strings.xml \
-c:\RoRDev\pxbMobile\platforms\android\res\xml\config.xml \
-c:\RoRDev\pxbMobile\platforms\android\bin\AndroidManifest.xml \
